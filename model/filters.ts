d25988bc9828bed9d68444153f40209567f0b38f
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-27 17:38:21 +0100

    Create filters.ts

---
+++ model/filters.ts
@@ -0,0 +1,42 @@
+import { Marked as Markdown } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+import { type ContentFragment } from "./ContentFragment.ts";
+import { Context } from "./Context.ts";
+import { createContentFragment } from "./createContentFragment.ts";
+
+
+export type FilterType<T = unknown> = (ctx: Context, content: T) => ContentFragment | string;
+
+
+export const noescape: FilterType<string> = (ctx, content: string) => {
+    return createContentFragment(ctx, content);
+};
+
+
+export const json: FilterType<unknown> = (_ctx, content) => {
+    return createContentFragment(Context.JS, JSON.stringify(content));
+};
+
+
+export const markdown: FilterType<string> = (_ctx, s) => {
+    return createContentFragment(Context.HTML, Markdown.parse(s).content);
+}
+
+
+export const trim: FilterType<string> = (_ctx, s) => {
+    return s.trim();
+}
+
+
+export const lower: FilterType<string> = (_ctx, s) => {
+    return s.toLowerCase();
+}
+
+
+export const upper: FilterType<string> = (_ctx, s) => {
+    return s.toUpperCase();
+}
+
+
+export const firstUpper: FilterType<string> = (_ctx, s) => {
+    return s.substring(0, 1).toUpperCase() + s.substring(1);
+}
