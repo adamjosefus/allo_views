9b1d9ef3ee63a4803d8f8d17fd9fed0282cadf38
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-27 16:10:32 +0100

    Rename `ContentPart` → `ContentFragment`

--- model/ContentFragment.ts
+++ model/ContentFragment.ts
@@ -1,18 +1,24 @@
-export abstract class ContentPart {
+export abstract class ContentFragment {
     protected _bases: string[];
-    protected _values: any[];
+    protected _values: unknown[];
 
+
+    // deno-lint-ignore no-explicit-any
     constructor(bases: string[] | string, values: any[] = []) {
         this._bases = typeof bases == 'string' ? [bases] : bases;
         this._values = values;
     }
 
+
+    // deno-lint-ignore no-explicit-any
     abstract escape(s: any): string;
+
 
     abstract toString(): string;
 }
 
 
 export type contextTagType = {
-    (contents: TemplateStringsArray, ...expressions: any[]): ContentPart
+    // deno-lint-ignore no-explicit-any
+    (contents: TemplateStringsArray, ...expressions: any[]): ContentFragment
 }
