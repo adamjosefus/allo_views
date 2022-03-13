357328f110c1713ac9578ed393ee4fa862679610
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-03-13 17:48:33 +0100

    Update demo.ts

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,20 +1,27 @@
+import { fromFileUrl, dirname, join } from "https://deno.land/std@0.129.0/path/mod.ts";
 import { RouterList } from "https://deno.land/x/allo_routing@v1.1.4/mod.ts";
 import { Server } from "https://deno.land/x/allo_server@v1.0.1/mod.ts";
 import { TemplateEngine } from "../mod.ts";
 
 
+const __dirname = dirname(fromFileUrl(import.meta.url));
+
+
 const port = 8080;
+const templateDir = join(__dirname, "templates");
 
 const engine = new TemplateEngine();
 
 
 const router = new RouterList();
-router.add("", (req: Request, params: Record<string, string>) => {
-    const s = engine.render("./templates/page.html", { foo: "Bar" });
+router.add("", (_req: Request, _params: Record<string, string>) => {
+    const templatePath = join(templateDir, "page.html");
+    const s = engine.render(templatePath, { foo: "Foo!" });
+
     return new Response(s);
 });
 
 const server = new Server(router);
-server.listen({port})
+server.listen({ port })
 
 console.log(`http://localhost:${port}`);
