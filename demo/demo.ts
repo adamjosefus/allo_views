6c9b8853ab1230a5c3289cbf019c89a8ba45ed2b
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-03-13 13:34:28 +0100

    Add demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,15 +1,17 @@
 import { RouterList } from "https://deno.land/x/allo_routing@v1.1.4/mod.ts";
 import { Server } from "https://deno.land/x/allo_server@v1.0.1/mod.ts";
-import { TemplateFactory } from "../mod.ts";
+import { TemplateEngine } from "../mod.ts";
 
 
 const port = 8080;
 
-const factory = new TemplateFactory();
+const engine = new TemplateEngine();
+
 
 const router = new RouterList();
 router.add("", (req: Request, params: Record<string, string>) => {
-    return new Response("Hello world!");
+    const s = engine.render("./templates/page.html", { foo: "Bar" });
+    return new Response(s);
 });
 
 const server = new Server(router);
