f9358c8c0e1286cf2c2637facd2fa5f06756a274
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-03-13 18:19:07 +0100

    Clean up code

--- libs/compileTemplateFragment.ts
+++ libs/compileTemplateFragment.ts
@@ -1,31 +1,7 @@
 import { type ExpressionSerializeCallback } from "./ExpressionSerializeCallback.ts";
+import { generateJavascriptVariablesCode } from "./helpers/generateJavascriptVariablesCode.ts";
 
 const expressionParser = /\{\{((?<name>\w+)|(\=(?<stringQuote>["']?)(?<inline>.+)\4))(?<callable>\((?<callableArgs>.*)\))?(?<filters>(\|\w+(\:.+)*)*)?\}\}/g;
-
-
-function generateJavascriptVariablesCode(params: Record<string, unknown>): string {
-    function escape(v: unknown): string {
-        if (typeof v === 'function') return v.toString();
-        if (typeof v === 'number') return v.toString();
-        if (typeof v === 'string') return `"${v.replaceAll('\\', '\\\\').replaceAll('"', '\\"')}"`;
-
-        return JSON.stringify(v);
-    }
-
-    const arr = Object.entries(params);
-
-    type VariableArr = [string, (...args: unknown[]) => unknown][];
-    type FunctionArr = [string, unknown][];
-
-    const vars = arr.filter(([_n, v]) => typeof v !== 'function') as VariableArr;
-    const funcs = arr.filter(([_n, v]) => typeof v === 'function') as FunctionArr;
-
-    // Variables must be declared before functions
-    return [...vars, ...funcs]
-        .map(([n, v]) => `const ${n} = ${escape(v)};`)
-        .join('\n');
-}
-
 
 
 function createStringSerializer(rawString: string, quoteMark: string): ExpressionSerializeCallback {
---
+++ libs/helpers/generateJavascriptVariablesCode.ts
@@ -0,0 +1,25 @@
+/**
+ * @internal
+ */
+export function generateJavascriptVariablesCode(params: Record<string, unknown>): string {
+    function escape(v: unknown): string {
+        if (typeof v === 'function') return v.toString();
+        if (typeof v === 'number') return v.toString();
+        if (typeof v === 'string') return `"${v.replaceAll('\\', '\\\\').replaceAll('"', '\\"')}"`;
+
+        return JSON.stringify(v);
+    }
+
+    const arr = Object.entries(params);
+
+    type VariableArr = [string, (...args: unknown[]) => unknown][];
+    type FunctionArr = [string, unknown][];
+
+    const vars = arr.filter(([_n, v]) => typeof v !== 'function') as VariableArr;
+    const funcs = arr.filter(([_n, v]) => typeof v === 'function') as FunctionArr;
+
+    // Variables must be declared before functions
+    return [...vars, ...funcs]
+        .map(([n, v]) => `const ${n} = ${escape(v)};`)
+        .join('\n');
+}
