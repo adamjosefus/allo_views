dac72074121899af4253c47630b49d2e501a158f
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-04-02 14:26:03 +0200

    Update JsContextContent.ts

--- libs/JsContextContent.ts
+++ libs/JsContextContent.ts
@@ -7,34 +7,17 @@ import { ContextContent, type ContextContentTag } from "./ContextContent.ts";
 
 export class JsContextContent extends ContextContent {
     
-    escape(s: unknown): string {
-        return JSON.stringify(s, null, 4);
+    static escape(value: unknown): string {
+        return JSON.stringify(value, null, 4);
     }
 
 
-    toString(): string {
-        const acc: string[] = [];
-
-        for (let i = 0; i < this.parts.length; i++) {
-            const base = this.parts[i];
-            acc.push(base);
-
-            if (this.values[i] !== undefined) {
-                const value = this.values[i];
-
-                if (value instanceof JsContextContent) {
-                    acc.push(value.toString());
-                } else {
-                    acc.push(this.escape(value));
-                }
-            }
-        }
-
-        return acc.join('');
+    render(): string {
+        return JsContextContent.renderInContext(JsContextContent, this.strings, this.keys);
     }
 }
 
 
 export const js: ContextContentTag = (contents: TemplateStringsArray, ...expressions: unknown[]) => {
-    return new JsContextContent([...contents], expressions);
+    return new JsContextContent([...contents], [...expressions]);
 }
