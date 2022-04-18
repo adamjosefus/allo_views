0635100726aa2004b7d858b98bd798b218bd8499
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-04-18 16:21:15 +0200

    Update demo.ts

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,8 +1,4 @@
-import {
-  dirname,
-  fromFileUrl,
-  join,
-} from "https://deno.land/std@0.135.0/path/mod.ts";
+import { dirname, fromFileUrl, join } from "https://deno.land/std@0.135.0/path/mod.ts";
 import { RouterList } from "https://deno.land/x/allo_routing@v1.1.4/mod.ts";
 import { Server } from "https://deno.land/x/allo_server@v1.0.1/mod.ts";
 import { html, js, TemplateEngine } from "../mod.ts";
@@ -16,15 +12,15 @@ const engine = new TemplateEngine();
 
 const router = new RouterList();
 router.add("", (_req: Request, _params: Record<string, string>) => {
-  const templatePath = join(templateDir, "page.html");
-  const s = engine.render(templatePath, {
-    html_text: "<h1>Hello, world!</h1>",
-    html: html`<h1>Hello, world!</h1>`,
-    js_text: `(() => 123)()`,
-    js: js`(() => 123)()`,
-  });
+    const templatePath = join(templateDir, "page.html");
+    const s = engine.render(templatePath, {
+        html_text: "<h1>Hello, world!</h1>",
+        html: html`<h1>Hello, world!</h1>`,
+        js_text: `(() => 123)()`,
+        js: js`(() => 123)()`,
+    });
 
-  return new Response(s);
+    return new Response(s);
 });
 
 const server = new Server(router);
